6bc13e0d974440b5e678f9fdede6d322acb24be8
arubanov785-code <arubanov785@gmail.com>
2026-08-04 00:57:57 +0700

    Update PirateShipPlugin.java

--- src/main/java/PirateShipPlugin.java
+++ src/main/java/PirateShipPlugin.java
@@ -15,17 +15,14 @@ public class PirateShipPlugin extends JavaPlugin {
     public void onEnable() {
         instance = this;
         
-        // Автоматически сохраняем ship.schem из ресурсов плагина, если его нет в папке
         if (!new File(getDataFolder(), "ship.schem").exists()) {
             saveResource("ship.schem", false);
         }
 
         this.shipManager = new ShipManager();
         
-        // Регистрация событий
         getServer().getPluginManager().registerEvents(new EventListener(), this);
 
-        // Регистрация команды /pirateship (или /ps)
         if (getCommand("pirateship") != null) {
             getCommand("pirateship").setExecutor(new ShipCommand(shipManager));
         }
@@ -44,13 +41,13 @@ public class PirateShipPlugin extends JavaPlugin {
         
         Bukkit.getScheduler().runTaskTimer(this, () -> {
             
-            // Оповещение за 1 час (за 72000 тиков до спавна)
+            // Оповещение за 1 час
             Bukkit.getScheduler().runTaskLater(this, () -> {
                 Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', 
                     "&8[&c&lПИРАТЫ&8] &eКапитан Черная Борода поднимает паруса! Пиратский корабль прибудет через &c1 час&e!"));
             }, sixHoursInTicks - 72000L);
 
-            // Оповещение за 5 минут (за 6000 тиков до спавна)
+            // Оповещение за 5 минут
             Bukkit.getScheduler().runTaskLater(this, () -> {
                 Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', 
                     "&8[&c&lПИРАТЫ&8] &eНа горизонте виднеются черные паруса! Корабль бросит якорь через &c5 минут&e!"));
