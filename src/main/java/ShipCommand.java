68c372b7c0b2f37ca4af61d944be930b80aeabf8
arubanov785-code <arubanov785@gmail.com>
2026-08-03 23:52:50 +0700

    Update ShipCommand.java

--- src/main/java/ShipCommand.java
+++ src/main/java/ShipCommand.java
@@ -15,7 +15,6 @@ public class ShipCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        // Проверка прав (админ или консоль)
         if (!sender.hasPermission("pirateship.admin")) {
             sender.sendMessage(ChatColor.RED + "У вас нет прав на использование этой команды!");
             return true;
@@ -23,10 +22,7 @@ public class ShipCommand implements CommandExecutor {
 
         if (args.length > 0 && args[0].equalsIgnoreCase("spawn")) {
             sender.sendMessage(ChatColor.YELLOW + "Запуск принудительного спавна пиратского корабля...");
-            
-            // Вызываем спавн корабля
             shipManager.spawnShipEvent();
-            
             sender.sendMessage(ChatColor.GREEN + "Ивент успешно запущен!");
             return true;
         }
