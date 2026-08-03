a348256fa4890324fb4e682eb1f0a26bb27d35a3
arubanov785-code <arubanov785@gmail.com>
2026-08-03 23:51:22 +0700

    Update PirateShipPlugin.java

--- src/main/java/PirateShipPlugin.java
+++ src/main/java/PirateShipPlugin.java
@@ -15,13 +15,20 @@ public class PirateShipPlugin extends JavaPlugin {
     public void onEnable() {
         instance = this;
         
-        // Автоматически сохраняем ship.schem из ресурсов плагина, если его нет
+        // Автоматически сохраняем ship.schem из ресурсов плагина, если его нет в папке
         if (!new File(getDataFolder(), "ship.schem").exists()) {
             saveResource("ship.schem", false);
         }
 
         this.shipManager = new ShipManager();
+        
+        // Регистрация событий
         getServer().getPluginManager().registerEvents(new EventListener(), this);
+
+        // Регистрация команды /pirateship (или /ps)
+        if (getCommand("pirateship") != null) {
+            getCommand("pirateship").setExecutor(new ShipCommand(shipManager));
+        }
 
         startEventTimer();
         getLogger().info("Плагин PirateShipEvent успешно запущен!");
@@ -32,19 +39,18 @@ public class PirateShipPlugin extends JavaPlugin {
     }
 
     private void startEventTimer() {
-        // 6 часов = 432 000 тиков (6 * 60 * 60 * 20)
+        // 6 часов = 432 000 тиков
         long sixHoursInTicks = 432000L;
         
-        // Циклический таймер раз в 6 часов
         Bukkit.getScheduler().runTaskTimer(this, () -> {
             
-            // Оповещение за 1 час (за 3600 сек до спавна)
+            // Оповещение за 1 час (за 72000 тиков до спавна)
             Bukkit.getScheduler().runTaskLater(this, () -> {
                 Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', 
                     "&8[&c&lПИРАТЫ&8] &eКапитан Черная Борода поднимает паруса! Пиратский корабль прибудет через &c1 час&e!"));
             }, sixHoursInTicks - 72000L);
 
-            // Оповещение за 5 минут (за 300 сек до спавна)
+            // Оповещение за 5 минут (за 6000 тиков до спавна)
             Bukkit.getScheduler().runTaskLater(this, () -> {
                 Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', 
                     "&8[&c&lПИРАТЫ&8] &eНа горизонте виднеются черные паруса! Корабль бросит якорь через &c5 минут&e!"));
